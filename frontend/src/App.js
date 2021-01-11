e606812866ecfba1dba63af70d4ec8b9699209a9
creinken <reinkendigital@gmail.com>
2021-01-11 12:36:53 -0600

    created initial backbone for app and blank home and login pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/Navbar'
+import Home from './containers/Home';
+import Login from './containers/Login';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  render() {
+    return (
+      <Router>
+          <>
+            <Navbar />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+          </>
+      </Router>
+    );
+  }
 }
 
-export default App;
+export default App
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () =>
+    <div className="navbar">
+        <NavLink
+            to="/"
+            exact
+            activeStyle={{
+                background: 'darkred'
+            }}
+            >Home</NavLink>
+            <NavLink
+                to="/login"
+                exact
+                activeStyle={{
+                    background: 'darkred'
+                }}
+            >Login</NavLink>
+    </div>;
+
+export default Navbar
