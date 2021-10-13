bce10ebbdc7e18f981f3f590d25aa62e33849bae
Christopher Reinken <reinkendigital@gmail.com>
2021-10-13 13:03:31 -0500

    created intruduction component

---
+++ frontend/src/components/Introduction.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+class Introduction extends Component {
+    render() {
+        return (
+            <Container>
+                <Row>
+                    <h6>Hi, my name is</h6>
+                </Row>
+                <Row>
+                    <h1>Christopher Reinken.</h1>
+                </Row>
+                <Row>
+                    <p>I'm a software engineer specializing in building
+                        high quality web applications in Rails and React.
+                    </p>
+                </Row>
+            </Container>
+        )
+    }
+}
+
+export default Introduction
--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -1,40 +1,13 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-import { Container, Row, Col } from 'react-bootstrap';
 import '../Home.css';
-import headShot from '../resources/man-475559_640.jpg';
+import Introduction from '../components/Introduction';
 
 class Home extends Component {
 
     render() {
         return (
-            <Container>
-                <Row>
-                    <Col md={6} sm={12}>
-                        <img id="binaryface" src={ headShot } />
-                    </Col>
-
-                    <Col md={6} sm={12}>
-                        <h1>Christopher Reinken</h1>
-                        <hr />
-
-                        <p class="text">Thank you for visiting. I am an independent Information Technology specialist with experience in
-                            anything from basic PC troubleshooting to network switching and routing to programming in React, Ruby, and
-                            others.</p>
-                    </Col>
-                </Row>
-
-                <Row>
-                    <Col xl={12}>
-                        <p class="text" id="secPara">I have been working with computers and home office computing for over 15 years, either for myself,
-                            friends and family, or for my grandfather’s home business. I am always continuing my education, and hope to
-                            break into the career that has been my passion for longer than I can remember. So please, take a look around. If you
-                            like what you see, you can <NavLink to="/contact" exact>contact me</NavLink> at any time. Have a great day!
-                        </p>
-                    </Col>
-                </Row>
-            </Container>
-
+            <Introduction/>
         )
     }
 }
